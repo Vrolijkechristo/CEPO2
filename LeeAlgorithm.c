c0d6d39020344cdc22f508a0127d86ceb1398c2b
Gyan van der Zijden <gyanvdz@gmail.com>
2023-05-04 12:02:38 +0200

    Made indentation of whole codebase somewhat more consistent
    Improved showtodo function to not show the many rows of -99 data. The showtodo function is quite useless since calcmaps is never in need of debugging, but it's still a tiny bit cool to have, I guess.
    
    Created mine detection system that automatically plans a new route keeping all previously found mines into account
    
    Integrated handlecell logic into calcmaps directly, removed handlecell.
    
    Since destinations are now based on stations, removed "invalid destination" if statement.

--- LeeAlgorithm.c
+++ LeeAlgorithm.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
-#include <time.h>
 
 #define M -1
 #define startmap   {{-1, -1, -1, -1,  0,  -1,  0, -1,  0, -1, -1, -1, -1},  \
                     {-1, -1, -1, -1,  0,  -1,  0, -1,  0, -1, -1, -1, -1},  \
-                    {-1, -1,  0,  0,  0,   0,  0,  M,  0,  0,  0, -1, -1},  \
+                    {-1, -1,  0,  0,  0,   0,  0,  0,  0,  0,  0, -1, -1},  \
                     {-1, -1,  0, -1,  0,  -1,  0, -1,  0, -1,  0, -1, -1},  \
-                    { 0,  0,  0,  0,  0,   0,  0,  M,  0,  0,  0,  0,  0},  \
-                    {-1, -1,  0, -1,  M,  -1,  M, -1,  0, -1,  0, -1, -1},  \
-                    { 0,  0,  0,  0,  0,   M,  0,  0,  0,  M,  0,  0,  0},  \
+                    { 0,  0,  0,  0,  0,   0,  0,  0,  0,  0,  0,  0,  0},  \
                     {-1, -1,  0, -1,  0,  -1,  0, -1,  0, -1,  0, -1, -1},  \
-                    { 0,  0,  0,  0,  0,   M,  0,  M,  0,  M,  0,  0,  0},  \
+                    { 0,  0,  0,  0,  0,   0,  0,  0,  0,  0,  0,  0,  0},  \
                     {-1, -1,  0, -1,  0,  -1,  0, -1,  0, -1,  0, -1, -1},  \
-                    {-1, -1,  0,  0,  0,   0,  0,  0,  0,  M,  0, -1, -1},  \
+                    { 0,  0,  0,  0,  0,   0,  0,  0,  0,  0,  0,  0,  0},  \
+                    {-1, -1,  0, -1,  0,  -1,  0, -1,  0, -1,  0, -1, -1},  \
+                    {-1, -1,  0,  0,  0,   0,  0,  0,  0,  0,  0, -1, -1},  \
                     {-1, -1, -1, -1,  0,  -1,  0, -1,  0, -1, -1, -1, -1},  \
                     {-1, -1, -1, -1,  0,  -1,  0, -1,  0, -1, -1, -1, -1}};
 
@@ -33,29 +32,30 @@ int map[13][13] = startmap;
 int dir[13][13] = startmap;
 int todo[90][2];
 
-int processedcells, posx, posy, endx, endy, stationstart, stationend;
+int processedcells, posx, posy, endx, endy, stationstart, stationend, olddir;
 int arrived = 0;
 
 void inputs() {
     printf("Robot position (station no.):  ");
     scanf("%d", &stationstart);
     printf("target position (station no.): ");
-    scanf("%d,", &stationend);
+    scanf("%d", &stationend);
 
     posy = stationmap[stationstart -1][0];
     posx = stationmap[stationstart -1][1];
     endy = stationmap[stationend   -1][0];
     endx = stationmap[stationend   -1][1];
 
-    printf("posy = %d,  posx = %d \n", posy, posx);
-    printf("endy = %d,  endx = %d \n", endy, endx);
+    printf("posy = %d,  posx = %d   \n",posy, posx);
+    printf("endy = %d,  endx = %d \n\n", endy, endx);
 
 }
+
 void showmap(int array[13][13]) { //shows the map's data (loveyou chisto for making this more efficient)
     int i, j;
-    printf("\n---------------------------------------");
+    printf("----------------------------------------------------\n");
     for(i=0; i<13; i++) {
-        printf("\n");
+
         for(j=0; j<13; j++) {
 
             if(array[i][j] == -1 || array[i][j] >9) {
@@ -63,7 +63,7 @@ void showmap(int array[13][13]) { //shows the map's data (loveyou chisto for mak
             else {
                 printf("  %d,", array[i][j]); }
         }
-
+        printf("\n");
     }
 
 }
@@ -71,21 +71,28 @@ void showmap(int array[13][13]) { //shows the map's data (loveyou chisto for mak
 void showtodo() { //shows sequential list of cells coords at which to evaluate neighbours
     int i, j;
     for(i=0; i<90; i++) {
-        printf("\n");
-        for(j=0; j<2; j++) {
 
-            printf(" %d", todo[i][j]); }
+        if(todo[i][0] != -99) {
+
+            for(j=0; j<2; j++) {
+
+                printf(" %d", todo[i][j]);
+            }
+
+            printf("\n");
+
+        }
 
     }
 
 }
 
-void handleneighbour(int cellx, int celly, int cellnum, int direction) {
+void handleneighbour(int cellx, int celly, int cellnum, int direction) { //valid neighbours recieve numbers based on active cell
 
-    if(map[celly][cellx] == 0 && -1 < cellx && cellx < 13 && -1 < celly && celly < 13) {
+    if(map[celly][cellx] == 0 && -1 < cellx && cellx < 13 && -1 < celly && celly < 13) { //only changes cells that are not walls and in bounds
 
         map[celly][cellx] = cellnum + 1;
-        dir[celly][cellx] = direction;
+        dir[celly][cellx] = direction; //1 means go north, 2-east, 3-south, 4-west
 
         processedcells++;
 
@@ -96,35 +103,37 @@ void handleneighbour(int cellx, int celly, int cellnum, int direction) {
 
 }
 
-void handlecell(int line) {
-
-    int y = todo[line][0];
-    int x = todo[line][1];
-
-    int cellnum = map[y][x];
-
-        handleneighbour(x, y +1, cellnum, 1);
-        handleneighbour(x +1, y, cellnum, 4);
-        handleneighbour(x, y -1, cellnum, 3);
-        handleneighbour(x -1, y, cellnum, 2);
-
-}
-
-int calcmaps(int endy, int endx) { //returns taxicab distance, fills map and dir
+int calcmaps(int endy, int endx) { //fills map and dir with values
 
     int line = 0;
+    processedcells = 0;
 
     todo[0][0] = endy;
     todo[0][1] = endx;
     map[endy][endx] = 1;
 
-    if(map[endy][endx] == -1) {
-        printf("invalid destination");
-    }
-
     while(map[posy][posx] == 0){
-        handlecell(line);
+
+        int y = todo[line][0];
+        int x = todo[line][1];
+
+        int cellnum = map[y][x];
+
+        handleneighbour(x, y +1, cellnum, 1); //when a cell is made south, store north to find the way back to the source
+        handleneighbour(x +1, y, cellnum, 4);
+        handleneighbour(x, y -1, cellnum, 3);
+        handleneighbour(x -1, y, cellnum, 2);
         line++;
+
+        //showmap(map);
+
+        if(line == 90) {
+
+            printf(" I'm stuck :(\n");
+            break;
+
+        }
+
     }
 
 }
@@ -139,28 +148,32 @@ int relative(int olddir, int posy, int posx) { //
         return 1000;
     }
 
-        if(newdir == olddir) {
-            printf("straight\n");
-            return 1011;
-        }
-        if(newdir == (olddir + 1) || newdir == (olddir - 3)) {
-            printf("right\n");
-            return 1001;
-        }
-        if(newdir == (olddir + 3) || newdir == (olddir -1)) {
-            printf("left\n");
-            return 1010;
-        }
-        if(newdir == (olddir + 2) || newdir == (olddir -2)) {
-            printf("turn180\n");
-            return 1100;
-        }
-
+    if(newdir == olddir) {
+        printf("straight\n");
+        return 1011;
+    }
+    if(newdir == (olddir + 1) || newdir == (olddir - 3)) {
+        printf("right\n");
+        return 1001;
+    }
+    if(newdir == (olddir + 3) || newdir == (olddir -1)) {
+        printf("left\n");
+        return 1010;
+    }
+    if(newdir == (olddir + 2) || newdir == (olddir -2)) {
+        printf("turn180\n");
+        return 1100;
+    }
 }
 
-void step(int steps) { //to be called when the robot reaches a midpoint
+void step(int steps, int minefound, int currentdir) { //to be called when the robot reaches a midpoint
 
-    int olddir = dir[posy][posx];
+    if(minefound == 1) {
+        olddir = currentdir;
+    }
+    else {
+        olddir = dir[posy][posx];
+    }
 
     switch (olddir) {
 
@@ -184,35 +197,109 @@ void step(int steps) { //to be called when the robot reaches a midpoint
             relative(olddir, posy, posx);
             //printf("west\n");
             break;
+
     }
+
 }
 
-    int main() {
+void mine() { //to be called when robot reaches a mine, stores mine pos. in maps, recalcs route, creates next direction command
 
-        inputs();
+    int currentdir = dir[posy][posx];
+    int i, j;
 
-        int i;
-        for (i = 0; i < 90; i++) {
-            todo[i][0] = -99;
-            todo[i][1] = -99;
+    for(i=0; i<13; i++) { //clear both maps
+
+        for (j = 0; j < 13; j++) {
+
+            if (map[i][j] != -1) { // keep all walls (thus already found mines) untouched
+                map[i][j] = 0;
+                dir[i][j] = 0;
+
+            }
         }
+    }
 
-        calcmaps(endy, endx); // y, x location of target
+    switch(currentdir) {
 
-        //showtodo();
-        //showmap(map);
-        //showmap(dir);
-        printf("\nprocessed cells: %d\n", processedcells);
-
-        //walk(posy , posx); //y , x location of robot
-
-        printf("%d, %d, ", posy, posx);
-        printf("straight\n");
-
-        while (arrived == 0) {
-            step(2);
-        }
-
-        printf("arrived\n");
+        case 1:
+            map[posy -1][posx] = -1;
+            dir[posy -1][posx] = -1;
+            printf("\nMine found at %d, %d while driving NORTH: \n", posy -1, posx);
+            break;
+        case 2:
+            map[posy][posx +1] = -1;
+            dir[posy][posx +1] = -1;
+            printf("\nMine found at %d, %d while driving EAST: \n", posy, posx +1);
+            break;
+        case 3:
+            map[posy +1][posx] = -1;
+            dir[posy +1][posx] = -1;
+            printf("\nMine found at %d, %d while driving SOUTH: \n", posy +1, posx);
+            break;
+        case 4:
+            map[posy][posx -1] = -1;
+            dir[posy][posx -1] = -1;
+            printf("\nMine found at %d, %d while driving WEST: \n", posy, posx -1);
+            break;
 
     }
+
+    for (i = 0; i < 90; i++) {
+        todo[i][0] = -99;
+        todo[i][1] = -99;
+    }
+
+    calcmaps(endy, endx);
+    //showtodo();
+    //showmap(map);
+    showmap(dir);
+    //printf("processed cells: %d\n", processedcells);
+
+    step(0, 1, currentdir);
+
+}
+
+int main() {
+
+    inputs();
+
+    int i;
+    for (i = 0; i < 90; i++) {
+        todo[i][0] = -99;
+        todo[i][1] = -99;
+    }
+
+    calcmaps(endy, endx); // y, x location of target
+
+    //showtodo();
+    //showmap(map);
+    //showmap(dir);
+    printf("processed cells: %d\n", processedcells);
+
+    printf("%d, %d, ", posy, posx);
+    printf("straight\n");
+
+        step(2,0,0);
+        mine();
+        step(2,0,0);
+        step(2,0,0);
+        step(2,0,0);
+        mine();
+        mine();
+        mine();
+        step(2,0,0);
+        mine();
+        step(2,0,0);
+        step(2,0,0);
+        step(2,0,0);
+        step(2,0,0);
+        step(2,0,0);
+        step(2,0,0);
+        step(2,0,0);
+        step(2,0,0);
+        step(2,0,0);
+
+
+    printf("stop\n");
+
+}
